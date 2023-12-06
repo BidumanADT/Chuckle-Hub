0f0f149efc68a8d279eaad9555d8eba9609cbdce
Aaron D. Tanner <sgtadt@gmail.com>
2023-12-06 08:51:01 -0600

    init new branch

--- back-end/comment_app/models.py
+++ back-end/comment_app/models.py
@@ -4,7 +4,7 @@ from joke_app.models import Joke
 
 # Create your models here.
 class Comment(models.Model):
-    content = models.TextField()
+    content = models.TextField(max_length=300)
     joke = models.ForeignKey(Joke, on_delete=models.CASCADE, null=True, related_name='comment_joke')
     author = models.ForeignKey(Client, on_delete=models.CASCADE, null=True, related_name='comment_author')
     created_at = models.DateTimeField()
--- back-end/user_app/models.py
+++ back-end/user_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Client(AbstractUser):
-    email = models.CharField(unique=True)
+    email = models.EmailField(unique=True)
     bio = models.TextField(default="A little about me...", null=True, blank=True)
     
     USERNAME_FIELD = 'email'
